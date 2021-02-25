8fdaa37c5848837696239da949d8cf15fc1c01e1
Nick Nicolosi <nnicolosi@singlestoneconsulting.com>
2021-02-25 18:15:17 -0500

    Use ResponseEntity from controller endpoints and return appropriate HTTP status codes

--- src/main/kotlin/com/interplanetarytravel/reservationsystem/controllers/DestinationController.kt
+++ src/main/kotlin/com/interplanetarytravel/reservationsystem/controllers/DestinationController.kt
@@ -1,19 +1,27 @@
 package com.interplanetarytravel.reservationsystem.controllers
 
+import com.interplanetarytravel.reservationsystem.dtos.DestinationDto
 import com.interplanetarytravel.reservationsystem.enums.Destination
 import com.interplanetarytravel.reservationsystem.utils.toDestinationDto
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController()
 @RequestMapping("/destination")
 class DestinationController {
 
     @GetMapping("")
-    fun listDestinations() = Destination.values().map { it.toDestinationDto() }
+    fun listDestinations(): ResponseEntity<List<DestinationDto>> {
+        return ResponseEntity(Destination.values().map { it.toDestinationDto() }, HttpStatus.OK)
+    }
 
     @GetMapping("{id}")
-    fun destination(@PathVariable id: String) = Destination.valueOf(id.toUpperCase()).toDestinationDto()
+    fun destination(@PathVariable id: String): ResponseEntity<DestinationDto> {
+        return if (Destination.values().any { it.name == id.toUpperCase() }) {
+            ResponseEntity(Destination.valueOf(id.toUpperCase()).toDestinationDto(), HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
 }
--- src/main/kotlin/com/interplanetarytravel/reservationsystem/controllers/SpacecraftController.kt
+++ src/main/kotlin/com/interplanetarytravel/reservationsystem/controllers/SpacecraftController.kt
@@ -3,6 +3,8 @@ package com.interplanetarytravel.reservationsystem.controllers
 import com.interplanetarytravel.reservationsystem.dtos.SpacecraftDto
 import com.interplanetarytravel.reservationsystem.enums.Spacecraft
 import com.interplanetarytravel.reservationsystem.utils.toSpacecraftDto
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,8 +15,16 @@ import org.springframework.web.bind.annotation.RestController
 class SpacecraftController {
 
     @GetMapping("")
-    fun listSpacecraft(): List<SpacecraftDto> = Spacecraft.values().map { it.toSpacecraftDto() }
+    fun listSpacecraft(): ResponseEntity<List<SpacecraftDto>> {
+        return ResponseEntity(Spacecraft.values().map { it.toSpacecraftDto() }, HttpStatus.OK)
+    }
 
     @GetMapping("{id}")
-    fun spacecraft(@PathVariable id: String) = Spacecraft.valueOf(id.toUpperCase()).toSpacecraftDto()
+    fun spacecraft(@PathVariable id: String): ResponseEntity<SpacecraftDto> {
+        return if (Spacecraft.values().any { it.name == id.toUpperCase() }) {
+            ResponseEntity(Spacecraft.valueOf(id.toUpperCase()).toSpacecraftDto(), HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
 }
